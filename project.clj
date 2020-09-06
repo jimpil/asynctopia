8c2430290d6040fad16d03dd62aa513734bc122d
Dimitrios Piliouras <jimpil1985@gmail.com>
2020-09-06 14:26:05 +0100

    update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,20 @@
-(defproject asynctopia "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject jimpil/asynctopia "0.2.4-SNAPSHOT"
+  :description "High-level `core.async` utilities"
   :url "https://github.com/jimpil/asynctopia"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.3.610"]]
-  :repl-options {:init-ns asynctopia.core})
+  :repl-options {:init-ns asynctopia.core}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ;["vcs" "push"]
+                  ]
+  :deploy-repositories [["releases" :clojars]] ;; lein release :patch
+  :signing {:gpg-key "jimpil1985@gmail.com"})
