8c7c87e342492c5855057d8e78b6997e7b779044
Dimitrios Piliouras <jimpil1985@gmail.com>
2020-09-06 14:27:00 +0100

    add :score for clojure.core

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jimpil/asynctopia"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/core.async "1.3.610"]]
   :repl-options {:init-ns asynctopia.core}
 
